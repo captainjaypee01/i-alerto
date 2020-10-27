e7d5f965c4f2619eadd8a756e699192a6208df3e
Ubuntu <ubuntu@ip-172-31-18-93.us-east-2.compute.internal>
2020-10-27 00:01:19 +0000

    live update

--- config/websockets.php
+++ config/websockets.php
@@ -116,13 +116,13 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
+        'local_cert' => '/etc/letsencrypt/live/ialerto.com/cert.pem',
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
+        'local_pk' => '/etc/letsencrypt/live/ialerto.com/privkey.pem',
 
         /*
          * Passphrase for your local_cert file.
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -20,9 +20,14 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 // window.Pusher = require('pusher-js');
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    wsHost: window.location.hostname,
+    wssHost: window.location.hostname,
+    wsPort: 6001,
+    wssPort:6001,
+    forceTLS: true,
+    disableStats: true,
+});
