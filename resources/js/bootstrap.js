12ebf99b9fb8eff84c8ffb9e9771bab2b10220de
bryanbernardo22 <bryanbernardo9828@gmail.com>
2020-10-27 13:51:40 +0800

    redis server

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -16,18 +16,11 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo';
+import Echo from "laravel-echo"
 
-// window.Pusher = require('pusher-js');
+window.io = require('socket.io-client');
 
 window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    wsHost: window.location.hostname,
-    wssHost: window.location.hostname,
-    wsPort: 6001,
-    wssPort:6001,
-    forceTLS: true,
-    disableStats: true,
+    broadcaster: 'socket.io',
+    host: window.location.hostname + ':6001'
 });
