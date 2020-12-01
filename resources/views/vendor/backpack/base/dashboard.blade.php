c4cef69447a18110e138076cf1a9416b8df7e2dd
John Paul Dala <jaypeedala31@gmail.com>
2020-12-01 18:41:48 +0800

    fix month

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -5,7 +5,7 @@
         <div class="col-sm-6 col-lg-3">
             <div class="card text-white bg-primary">
                 <div class="card-body pb-0">
-                    
+
                 <div class="text-value">{{ $month_users ?? 0 }}</div>
                 <div>Total New Users in {{ Carbon\Carbon::now()->monthName }} {{ Carbon\Carbon::now()->year }}</div>
                 </div>
@@ -18,11 +18,11 @@
             <div class="card text-white bg-info">
                 <div class="card-body pb-0">
                 <button class="btn btn-transparent p-0 float-right" type="button"><i class="icon-location-pin"></i></button>
-                <div class="text-value">{{ $total_announcements ?? 0 }}</div>
+                <div class="text-value">{{ $month_announcements ?? 0 }}</div>
                 <div>Total Announcements in {{ Carbon\Carbon::now()->monthName }} {{ Carbon\Carbon::now()->year }}</div>
                 </div>
                 <div class="chart-wrapper mt-3 mx-3" style="height:70px;"><div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
-                
+
                 </div>
             </div>
         </div>
@@ -44,7 +44,7 @@
             </div>
             <!-- /.row-->
             <div class="row">
-                
+
             </div>
 
         </div>
@@ -63,5 +63,5 @@
     <script src="https://backstrap.net/vendors/@coreui/coreui-plugin-chartjs-custom-tooltips/js/custom-tooltips.min.js"></script>
     <script src="https://backstrap.net/js/main.js"></script>
     {!! $dashboardChart->script() !!}
-    
+
 @endsection
